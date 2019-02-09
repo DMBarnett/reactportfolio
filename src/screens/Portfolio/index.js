4757586f7d0f6de2702e2342c59c1d0e7f7fc38b
DMBarnettLaptop <davidmitchellbarnett@gmail.com>
2019-02-09 12:59:20 -0500

    update to styling on portfolio page

--- src/screens/Portfolio/index.js
+++ src/screens/Portfolio/index.js
@@ -5,7 +5,49 @@ import "./style.css"
 function Portfolio(props){
   return(
     <div>
-      <Container>
+      <Container fluid>
+        <Row>
+          <Col sm="6" md="3">
+            <Card>
+              <CardBody>
+                <CardTitle><h4>Urza's Point of Sale</h4></CardTitle>
+                <CardText>This is a point of sale system for a store that sells Magic the Gathering cards.  It draws pricing from <a href="tcgplayer.com">TCG Player</a>, and maintains a database of the stores inventory and sales history.  Also, it allows for tracking of repeat customers who generate and use store credit through winning tournements</CardText>
+                <CardLink href="https://murmuring-shelf-36712.herokuapp.com/">Heroku Hosting</CardLink>
+                <CardLink href="https://github.com/DMBarnett/pointofsale">Code on Github</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="3">
+          <Card>
+              <CardBody>
+                <CardTitle><h4>Book tracking database</h4></CardTitle>
+                <CardText>This was a simple project to build an app that tracks a reading list for a user, and allows them to add and remove new books. </CardText>
+                <CardLink href="https://infinite-atoll-67913.herokuapp.com/">Heroku Hosting</CardLink>
+                <CardLink href="https://github.com/DMBarnett/searchgooglebooks">Code on Github</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="3">
+          <Card>
+              <CardBody>
+                <CardTitle>Urza's Point of Sale</CardTitle>
+                <CardText>This is a point of sale system for a store that sells Magic the Gathering cards.  It draws pricing from <a href="tcgplayer.com">TCG Player</a>, and maintains a database of the stores inventory and sales history.  Also, it allows for tracking of repeat customers who generate and use store credit through winning tournements</CardText>
+                <CardLink href="https://murmuring-shelf-36712.herokuapp.com/">Heroku Hosting</CardLink>
+                <CardLink href="https://github.com/DMBarnett/pointofsale">Code on Github</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col sm="6" md="3">
+          <Card>
+              <CardBody>
+                <CardTitle>Urza's Point of Sale</CardTitle>
+                <CardText>This is a point of sale system for a store that sells Magic the Gathering cards.  It draws pricing from <a href="tcgplayer.com">TCG Player</a>, and maintains a database of the stores inventory and sales history.  Also, it allows for tracking of repeat customers who generate and use store credit through winning tournements</CardText>
+                <CardLink href="https://murmuring-shelf-36712.herokuapp.com/">Heroku Hosting</CardLink>
+                <CardLink href="https://github.com/DMBarnett/pointofsale">Code on Github</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
         <Row>
           <Col sm="6" md="3">
             <Card>
@@ -48,12 +90,7 @@ function Portfolio(props){
             </Card>
           </Col>
         </Row>
-        <Row>
-
-        </Row>
-        <Row>
-
-        </Row>
+        
       </Container>
     </div>
   )
