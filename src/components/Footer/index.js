49305f67ffa82b78976f5e490a1812c170f90f3e
DMBarnettLaptop <davidmitchellbarnett@gmail.com>
2019-02-10 14:07:59 -0500

    footer finished

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer (){
   return (
     <div className="footer">
       <Container fluid>
-        <Row>
+        <Row  className="footRow">
           <Col className="leftCol justify-content-start" xs="12" sm="6">
             <a href="https://www.linkedin.com/in/davidmbarnett/"><h4>LinkedIn</h4></a>
             <a href="https://github.com/DMBarnett"><h4>Github</h4></a>
